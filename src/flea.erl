f2448e84d10a3d423fe7c36a0e887415a42b5627
niahoo <dev@ooha.in>
2014-09-15 17:38:36 +0200

    add handle_message/1 for to detect closes

--- src/flea.erl
+++ src/flea.erl
@@ -1,6 +1,6 @@
 -module(flea).
 -export([out/2]).
--export([init/1,handle_open/2,handle_message/2,handle_info/2,terminate/2]).
+-export([init/1,handle_open/2,handle_message/1,handle_message/2,handle_info/2,terminate/2]).
 
 -define(Y,yaws_api).
 -define(POLL_TIMEOUT, 30000).
